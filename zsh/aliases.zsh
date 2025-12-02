36a841ed0f5f9a9290f71642164de7e4d65cbd26
phil <belda.philippe@proton.me>
2025-12-02 18:56:29 +0100

    new shell aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,8 @@
 alias ec="emacsclient -c || emacs --daemon && emacsclient -c"
 alias ek="emacsclient -e \"(kill-emacs)\""
+
+# git
+alias ga    = "git add"
+alias gcmsg = "git commit -m"
+alias glog  = "git log --all --pretty --graph --oneline"
+alias gst   = "git status"
