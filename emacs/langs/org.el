e41e3315029d7e92e7fef696b1e50086a15e4900
phil <belda.philippe@proton.com>
2026-02-23 11:25:24 +0100

    better org mode

--- emacs/langs/org.el
+++ emacs/langs/org.el
@@ -1,54 +1,63 @@
 (use-package org-modern
   :ensure t
   :defer t
-  :hook (org-mode . org-modern-mode)
-  :config
-
-  ;; Resize Org headings
-  (dolist (face '((org-level-1 . 1.35)
-                  (org-level-2 . 1.3)
-                  (org-level-3 . 1.2)
-                  (org-level-4 . 1.1)
-                  (org-level-5 . 1.1)
-                  (org-level-6 . 1.1)
-                  (org-level-7 . 1.1)
-                  (org-level-8 . 1.1)))
-    )
-  ;; (set-face-attribute (car face) nil :font "Open Sans" :weight 'bold :height (cdr face)))
-
-  ;; Make the document title a bit bigger
-  ;; (set-face-attribute 'org-document-title nil :font "Open Sans" :weight
-  ;; 'bold :height 1.8)
-
-  (require 'org-indent)
-  (set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
+  :hook ((org-mode . org-modern-mode)
+         (org-modern-mode . olivetti-mode)
+         (org-modern-mode . org-toggle-inline-images))
 
 
-  (set-face-attribute 'org-block nil            :foreground 'unspecified :inherit
-                      'fixed-pitch :height 0.85)
-  (set-face-attribute 'org-code nil             :inherit '(shadow fixed-pitch) :height 0.85)
-  (set-face-attribute 'org-indent nil           :inherit '(org-hide fixed-pitch) :height 0.85)
-  (set-face-attribute 'org-verbatim nil         :inherit '(shadow fixed-pitch) :height 0.85)
-  (set-face-attribute 'org-special-keyword nil  :inherit '(font-lock-comment-face
-                                                           fixed-pitch))
-  (set-face-attribute 'org-meta-line nil        :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-checkbox nil         :inherit 'fixed-pitch)
 
-  (add-hook 'org-mode-hook 'variable-pitch-mode)
+  ;; :config
 
-  (plist-put org-format-latex-options :scale 2)
+  ;; ;; Resize Org headings
+  ;; (dolist (face '((org-level-1 . 1.35)
+  ;;                 (org-level-2 . 1.3)
+  ;;                 (org-level-3 . 1.2)
+  ;;                 (org-level-4 . 1.1)
+  ;;                 (org-level-5 . 1.1)
+  ;;                 (org-level-6 . 1.1)
+  ;;                 (org-level-7 . 1.1)
+  ;;                 (org-level-8 . 1.1)))
+  ;;   )
+  ;; ;; (set-face-attribute (car face) nil :font "Open Sans" :weight 'bold :height (cdr face)))
 
-  (setq org-adapt-indentation t
-        org-hide-leading-stars t
-        org-hide-emphasis-markers t
-        org-pretty-entities t
-	    org-ellipsis "  ·")
+  ;; ;; Make the document title a bit bigger
+  ;; ;; (set-face-attribute 'org-document-title nil :font "Open Sans" :weight
+  ;; ;; 'bold :height 1.8)
 
-  (setq org-src-fontify-natively t
-	    org-src-tab-acts-natively t
-        org-edit-src-content-indentation 0)
+  ;; (require 'org-indent)
+  ;; (set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
 
-  (add-hook 'org-mode-hook 'visual-line-mode)
-  (add-hook 'org-mode-hook 'olivetti-mode)
-  (add-hook 'org-mode-hook 'org-indent-mode)
+
+  ;; (set-face-attribute 'org-block nil            :foreground 'unspecified :inherit
+  ;;                     'fixed-pitch :height 0.85)
+  ;; (set-face-attribute 'org-code nil             :inherit '(shadow fixed-pitch) :height 0.85)
+  ;; (set-face-attribute 'org-indent nil           :inherit '(org-hide fixed-pitch) :height 0.85)
+  ;; (set-face-attribute 'org-verbatim nil         :inherit '(shadow fixed-pitch) :height 0.85)
+  ;; (set-face-attribute 'org-special-keyword nil  :inherit '(font-lock-comment-face
+  ;;                                                          fixed-pitch))
+  ;; (set-face-attribute 'org-meta-line nil        :inherit '(font-lock-comment-face fixed-pitch))
+  ;; (set-face-attribute 'org-checkbox nil         :inherit 'fixed-pitch)
+
+  ;; (add-hook 'org-mode-hook 'variable-pitch-mode)
+
+  ;; (plist-put org-format-latex-options :scale 2)
+
+  ;; (setq org-adapt-indentation t
+  ;;       org-hide-leading-stars t
+  ;;       org-hide-emphasis-markers t
+  ;;       org-pretty-entities t
+  ;;       org-ellipsis "  ·")
+
+  ;; (setq org-src-fontify-natively t
+  ;;       org-src-tab-acts-natively t
+  ;;       org-edit-src-content-indentation 0)
+
+  ;; (add-hook 'org-mode-hook 'visual-line-mode)
+  ;; (add-hook 'org-mode-hook 'olivetti-mode)
+  ;; (add-hook 'org-mode-hook 'org-indent-mode)
   )
+
+(use-package olivetti
+  :ensure t
+  :hook (org-mode . olivetti-mode))
