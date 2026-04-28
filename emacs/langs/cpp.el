19e574d109204cd747099b9be854c3a77f494d14
phil <belda.philippe@proton.com>
2026-04-28 18:23:32 +0200

    adding .ipp, .tpp

--- emacs/langs/cpp.el
+++ emacs/langs/cpp.el
@@ -1,7 +1,7 @@
 ;; c++-ts-mode packaged with emacs
 (use-package c++-ts-mode
   :defer t
-  :mode ("\\.cpp\\'" "\\.c++\\'" "\\.cxx\\'" "\\.hpp\\'" "\\.hxx\\'")
+  :mode ("\\.cpp\\'" "\\.c++\\'" "\\.cxx\\'" "\\.hpp\\'" "\\.hxx\\'" "\\.tpp\\'" "\\.txx\\'""\\.ipp\\'" "\\.ixx\\'")
   :config
   ;; dont know why but c grammar needed
   (ts-add-lang 'c "https://github.com/tree-sitter/tree-sitter-c")
